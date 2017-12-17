75bea5d570fcd606cec6d0f0d1e4d2cbefb2e636
Pinkfloyd92 <ascacere@espol.edu.ec>
2017-12-16 23:22:51 -0500

    Removed auth files

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userRoutes = require('./user.route');
-const authRoutes = require('./auth.route');
 
 const router = express.Router();
 
@@ -9,12 +8,6 @@ const router = express.Router();
  */
 router.get('/status', (req, res) => res.send('OK'));
 
-/**
- * GET v1/docs
- */
-router.use('/docs', express.static('docs'));
-
 router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
 
 module.exports = router;
