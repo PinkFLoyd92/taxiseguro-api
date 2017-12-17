dbe0240342587f2936fb461bdd58e51a04428c8d
Pinkfloyd92 <ascacere@espol.edu.ec>
2017-12-17 01:02:01 -0500

    Added missing schemas

---
+++ src/api/models/fence.model.js
@@ -0,0 +1,72 @@
+const GeoJSON = require('mongoose-geojson-schema');
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const { omitBy, isNil } = require('lodash');
+const APIError = require('../utils/APIError');
+
+/**
+ * Fence Schema
+ * @private
+ */
+const fenceSchema = new mongoose.Schema({
+  route: { type: mongoose.Schema.Types.ObjectId, ref: 'Route' },
+  polygon: mongoose.Schema.Types.Polygon,
+}, {
+  timestamps: true,
+});
+
+/**
+ * Statics
+ */
+fenceSchema.statics = {
+
+  /**
+   * Get fence
+   *
+   * @param {ObjectId} id - The objectId of fence.
+   * @returns {Promise<Fence, APIError>}
+   */
+  async get(id) {
+    try {
+      let fence;
+
+      if (mongoose.Types.ObjectId.isValid(id)) {
+        fence = await this.findById(id).exec();
+      }
+      if (fence) {
+        return fence;
+      }
+
+      throw new APIError({
+        message: 'Fence does not exist',
+        status: httpStatus.NOT_FOUND,
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  /**
+   * List fences in descending order of 'createdAt' timestamp.
+   *
+   * @param {number} skip - Number of fences to be skipped.
+   * @param {number} limit - Limit number of fences to be returned.
+   * @returns {Promise<Fence[]>}
+   */
+  list({
+    page = 1, perPage = 30, route,
+  }) {
+    const options = omitBy({ route }, isNil);
+
+    return this.find(options)
+      .sort({ createdAt: -1 })
+      .skip(perPage * (page - 1))
+      .limit(perPage)
+      .exec();
+  },
+};
+
+/**
+ * @typedef Fence
+ */
+module.exports = mongoose.model('Fence', fenceSchema);
---
+++ src/api/models/route.model.js
@@ -0,0 +1,86 @@
+const GeoJSON = require('mongoose-geojson-schema');
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const { omitBy, isNil } = require('lodash');
+const APIError = require('../utils/APIError');
+
+/**
+* Route Status
+*/
+const possibleStatus = ['inactive', 'active', 'cancelled', 'finished'];
+
+/**
+ * Route Schema
+ * @private
+ */
+const routeSchema = new mongoose.Schema({
+  driver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  client: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  start: { type: Date },
+  points: mongoose.Schema.Types.LineString,
+  status: {
+    type: String,
+    enum: possibleStatus,
+    default: 'inactive',
+  },
+}, {
+  timestamps: true,
+});
+
+/**
+ * Statics
+ */
+routeSchema.statics = {
+
+  possibleStatus,
+
+  /**
+   * Get route
+   *
+   * @param {ObjectId} id - The objectId of route.
+   * @returns {Promise<Route, APIError>}
+   */
+  async get(id) {
+    try {
+      let route;
+
+      if (mongoose.Types.ObjectId.isValid(id)) {
+        route = await this.findById(id).exec();
+      }
+      if (route) {
+        return route;
+      }
+
+      throw new APIError({
+        message: 'Route does not exist',
+        status: httpStatus.NOT_FOUND,
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  /**
+   * List routes in descending order of 'createdAt' timestamp.
+   *
+   * @param {number} skip - Number of routes to be skipped.
+   * @param {number} limit - Limit number of routes to be returned.
+   * @returns {Promise<Route[]>}
+   */
+  list({
+    page = 1, perPage = 30, name, status,
+  }) {
+    const options = omitBy({ name, status }, isNil);
+
+    return this.find(options)
+      .sort({ createdAt: -1 })
+      .skip(perPage * (page - 1))
+      .limit(perPage)
+      .exec();
+  },
+};
+
+/**
+ * @typedef Route
+ */
+module.exports = mongoose.model('Route', routeSchema);
