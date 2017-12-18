4530a9a4d91ef5105ef99dcbe08d20a6575838a3
Sebastián Cáceres <ascacere92@gmail.com>
2017-12-17 23:58:41 -0500

    Starter Bundle (#1)
    * Removed auth files
    
    * Fixed mongodb configuration

--- src/api/validations/user.validation.js
+++ src/api/validations/user.validation.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const User = require('../models/user.model');
+const User = require('../models/user.model'); // driver or client
 
 module.exports = {
 
@@ -7,19 +7,18 @@ module.exports = {
   listUsers: {
     query: {
       page: Joi.number().min(1),
-      perPage: Joi.number().min(1).max(100),
+      perPage: Joi.number().min(1).max(20),
       name: Joi.string(),
-      email: Joi.string(),
       role: Joi.string().valid(User.roles),
+      active: Joi.boolean(),
     },
   },
 
   // POST /v1/users
   createUser: {
     body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(128).required(),
       name: Joi.string().max(128),
+      password: Joi.string().min(6).max(128).required(),
       role: Joi.string().valid(User.roles),
     },
   },
@@ -27,9 +26,8 @@ module.exports = {
   // PUT /v1/users/:userId
   replaceUser: {
     body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(128).required(),
       name: Joi.string().max(128),
+      password: Joi.string().min(6).max(128).required(),
       role: Joi.string().valid(User.roles),
     },
     params: {
@@ -40,9 +38,8 @@ module.exports = {
   // PATCH /v1/users/:userId
   updateUser: {
     body: {
-      email: Joi.string().email(),
-      password: Joi.string().min(6).max(128),
       name: Joi.string().max(128),
+      password: Joi.string().min(6).max(128),
       role: Joi.string().valid(User.roles),
     },
     params: {
