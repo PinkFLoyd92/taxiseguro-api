20fd287097a5708b7f4c178030aa0d90872c12f8
Pinkfloyd92 <ascacere@espol.edu.ec>
2017-12-25 16:40:19 -0500

    Seeding route, missing info

--- src/config/seeders.js
+++ src/config/seeders.js
@@ -1,11 +1,41 @@
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+
 const seeder = require('mongoose-seed');
 const { mongo } = require('./vars');
+const User = require('../api/models/user.model');
+const Route = require('../api/models/route.model');
 
+function seedRoute(driverName, clientName, info = {}) {
+  return new Promise((resolve) => {
+    User.findOne({ name: driverName }).exec()
+      .then((user1) => {
+        User.findOne({ name: clientName }).exec()
+          .then((user2) => {
+            // console.info(user2);
+            Route.create({ driver: user1._id, client: user2._id }).then((route) => {
+              resolve(route);
+            }, (error) => {
+              console.error(error);
+            });
+          }, (error) => {
+            console.error(error);
+          });
+      }, (error) => {
+        console.error(error);
+      });
+  });
+}
+
+async function populateAll(seeder) {
+  await Promise.all([seedRoute('Usuario 1', 'Usuario 2', {})]).then(() => {
+    seeder.disconnect();
+  });
+}
 // Connect to MongoDB via Mongoose
 seeder.connect(mongo.uri, () => {
   // Load Mongoose models
   seeder.loadModels([
-    'src//api/models/user.model.js',
+    'src/api/models/user.model.js',
     'src/api/models/route.model.js',
     'src/api/models/fence.model.js',
   ]);
@@ -14,7 +44,7 @@ seeder.connect(mongo.uri, () => {
   seeder.clearModels(['User', 'Route', 'Fence'], () => {
     // Callback to populate DB once collections have been cleared
     seeder.populateModels(data, () => {
-      seeder.disconnect();
+      populateAll(seeder);
     });
   });
 });
