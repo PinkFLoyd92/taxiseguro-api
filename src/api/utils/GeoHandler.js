d6cf221e3611981f3a81782bd1738941382bd9c3
Sebastián Cáceres <ascacere92@gmail.com>
2018-02-04 21:14:26 -0500

    Letting just the client activate the route

--- src/api/utils/GeoHandler.js
+++ src/api/utils/GeoHandler.js
@@ -59,7 +59,7 @@ exports.checkRouteStatus = async (io, monitors = [], data = {}) => {
     if (route.status === 'active' || route.status === 'danger') {
       console.info('ROUTE IS ALREADY ACTIVATED');
       canRouteFinish(clientPos, driverPos, route, io, monitors);
-    } else if (route.status === 'pending') {
+    } else if (route.status === 'pending' && data.role === 'client') {
       console.info('ROUTE IS NOT ACTIVATED YET');
       canRouteActivate(clientPos, driverPos, route, io, monitors);
     }
